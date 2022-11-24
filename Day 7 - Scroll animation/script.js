a20991535bacfb18da86dd2dba86287c76c1a349
Dahman <94912743+DahmanCode@users.noreply.github.com>
2022-11-24 17:17:01 +0100

    Day 7 Project 7

---
+++ Day 7 - Scroll animation/script.js
@@ -0,0 +1,19 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 6 * 4.5
+
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top
+
+    if(boxTop < triggerBottom) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
+}
