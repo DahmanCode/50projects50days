e8122b3f50020c3d743cac036b830f2139bd85b1
Dahman <94912743+DahmanCode@users.noreply.github.com>
2022-12-13 16:15:21 +0100

    Day 26 - Project 26

--- Day 26 - Toast Notification/script.js
+++ Day 26 - Toast Notification/script.js
@@ -1,2 +1,39 @@
-const nav = document.querySelector('.nav')
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
 
+const messages = [
+  'Message One',
+  'Message Tow',
+  'Message Three',
+  'Message Four'
+]
+
+const types = [
+  'info',
+  'success',
+  'error'
+]
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification() {
+  const notif = document.createElement('div')
+  notif.classList.add('toast')
+  notif.classList.add(getRandomType())
+
+  notif.innerText = getRandomMessage()
+
+  toasts.appendChild(notif)
+
+  setTimeout(() => {
+    notif.remove()
+  }, 3000)
+}
+
+function getRandomMessage() {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)]
+}
